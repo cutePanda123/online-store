91369f8f716ce6397c7063ee70070480d292b541
Peter Li <peterli1234@126.com>
2020-12-12 19:29:04 -0800

    1. added tomcat accesslog config 2. added redis cache to save distributed session

---
+++ src/main/java/com/imooc/seckill/config/RedisConfig.java
@@ -0,0 +1,8 @@
+package com.imooc.seckill.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+public class RedisConfig { }
