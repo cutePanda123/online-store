a55ea1ba53554f19ccede4b4e83b4879d5b3d8e6
Peter Li <peterli1234@126.com>
2020-12-13 17:25:40 -0800

    fixed redis session pom dependencies.

--- src/main/java/com/imooc/seckill/config/SpringSessionConfig.java
+++ src/main/java/com/imooc/seckill/config/SpringSessionConfig.java
@@ -2,8 +2,6 @@ package com.imooc.seckill.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
