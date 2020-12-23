82b71c596ab2b8a2ff9591cab1852b30e19e1098
Peter Li <peterli1234@126.com>
2020-12-23 15:43:35 -0800

    added rocketmq deployment files

---
+++ deploy/rocketmq/rocketmq.sh
@@ -0,0 +1,4 @@
+
+sudo nohup sh bin/mqnamesrv &
+sudo nohup sh bin/mqbroker -n 127.0.0.1:9876 -c conf/broker.conf autoCreateTopicEnable=true &
+./mqadmin updateTopic -n 127.0.0.1:9876 -t stock -c DefaultCluster
